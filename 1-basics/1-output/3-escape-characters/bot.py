893eae571f1c28031a2213971d3fdebeadd9673e
Pedro de Almeida <4dealp21@solent.ac.uk>
2019-10-02 22:07:48 +0000

    Added code to display escape characters.

---
+++ 1-basics/1-output/3-escape-characters/bot.py
@@ -0,0 +1,6 @@
+# Display escape characters
+print("\n")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
