fef6a95e0f27a69ce6a2afc161ad74fe5499c3d8
Pedro de Almeida <4dealp21@solent.ac.uk>
2019-10-03 10:33:58 +0000

    Added code to demonstrate the use of a nested decision statement.

---
+++ 1-basics/3-decision/7-nested-decisions/bot.py
@@ -0,0 +1,21 @@
+# Ask user what type of cover does the book have
+print("What type of cover does the book have (soft/hard)?")
+cover = input()
+
+# Decide between soft and hard
+if (cover == "soft"):
+
+    #Get user input
+    #Decide between perfect-bound or not
+    print("Is the book perfect-bound (yes/not)?")
+    bound = input()
+
+    if (bound == "yes"):
+        print("Soft cover, perfect bound books are very popular!")
+
+    else:
+        print("Soft covers with coils or stitches are great for short books")
+
+else:
+    print("Books with hard covers can be more expensive!")
+
