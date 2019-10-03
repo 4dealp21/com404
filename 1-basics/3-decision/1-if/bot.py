9cc41e25fc216c65987afe6c9775ba8f909ac593
Pedro de Almeida <4dealp21@solent.ac.uk>
2019-10-03 08:48:05 +0000

    Added code to demonstrate the use of an if...else statement.

--- 1-basics/3-decision/1-if/bot.py
+++ 1-basics/3-decision/1-if/bot.py
@@ -2,6 +2,7 @@
 print("What type of book is this?")
 book_type = input()
 
+#Test and display if the book is an adventure book or not
 if(book_type == "adventure"):
     print("I like adventure books!") 
 
