25226cc7d7f65e0a4a4c4d401f3fa0015ce2e2f1
Pedro de Almeida <4dealp21@solent.ac.uk>
2019-10-09 12:54:05 +0000

    Added code to demonstrate the use of a logical and operator.

---
+++ 1-basics/3-decision/10-and-operator/bot.py
@@ -0,0 +1,14 @@
+#Get user input
+print("What did I hear?")
+sound = input()
+
+print("What did I see?")
+see = input()
+
+#Evaluate if Beep should or souldn't go away
+#Display results
+if((sound == "grrr") and (see == "two red eyes")):
+    print("There is a scary creature. I should get out of here!")
+
+else:
+    print("I am a little scared but I will continue.")
