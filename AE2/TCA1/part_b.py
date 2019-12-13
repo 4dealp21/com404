5d44ab7fcf740196004523dd12a9645f4ba27c65
Pedro de Almeida <4dealp21@solent.ac.uk>
2019-12-13 08:31:43 +0000

    Partb

--- AE2/TCA1/part_b.py
+++ AE2/TCA1/part_b.py
@@ -1,80 +1,132 @@
-#import tkinter
-
 from tkinter import *
 from tkinter import messagebox
 
-class TCA(Tk):
-
+class Gui(Tk):
     def __init__(self):
         super().__init__()
 
-        self.field_image = PhotoImage(file="default.gif")
-        self.field_image_green = PhotoImage(file="filled.gif")
-        self.field_image_red = PhotoImage(file="empty.gif")
+        # load resources
+        self.red_image = PhotoImage(file="red.gif")
+        self.green_image = PhotoImage(file="green.gif")
+        self.grey_image = PhotoImage(file="grey.gif")
 
-        #set window propreties
+        #options menu
+        self.options= StringVar()
+        self.options.set("Weekly")
+
+        #set window attributes
         self.title("Newsletter")
-        self.configure(bg="#ccc", padx=10, pady=10)
+        self.configure( bg= "#eee",
+                        padx= 10,
+                        pady= 10)
 
-        self.add_outer_frame()
-        self.add_heading_label()
-        self.add_instruction_label()
-        self.add_email_label()
-        self.add_email_entry()
-        self.add_field_image_label()
-        self.add_subscribe_button()
+        #add components
+        self.__add_heading_label()
+        self.__add_instructional_label()
+        self.__add_email_frame()
+        self.__add_email_label()
+        self.__add_email_entry()
+        self.__add_image_label()
+        self.__add_type_frame()
+        self.__add_type_label()
+        self.__add_type_optionmenu()
+        self.__add_subscribe_button()
+        
 
-    def add_outer_frame(self):
-        self.outer_frame = Frame()
-        self.outer_frame.grid(row=0, column=0)
-        self.outer_frame.configure(bg="#eee")
+    def __add_heading_label(self):
+        #create
+        self.heading_label= Label()
+        self.heading_label.grid(row=0, columnspan=3, sticky=N+E+S+W)
+        #style
+        self.heading_label.configure( font="Arial 14",
+                                     text="RECEIVE OUR NEWSLETTER",
+                                     pady=10)
+    def __add_instructional_label(self):
+        #create
+        self.instructional_label= Label()
+        self.instructional_label.grid(row=1, columnspan=3, sticky=N+E+S+W)
+        #style
+        self.instructional_label.configure(text="Please enter you email below to receive our newsletter.",
+                                           padx=10,
+                                           pady=10,
+                                           justify= "left")
+        
+        #handle events
+
+    def __add_email_frame(self):
+        self.email_frame= Frame()
+        self.email_frame.grid(row=2,columnspan=3, sticky= N+W+S+E)
+
+    def __add_email_label(self):
+        #create
+        self.email_label= Label(self.email_frame)
+        self.email_label.grid(row=2, column=0, padx=10, sticky=W)
+        #style
+        self.email_label.configure(text="Email:", padx=10)
+        #handle events
+
+    def __add_email_entry(self):
+        #create
+        self.email_entry= Entry(self.email_frame)
+        self.email_entry.grid(row=2, column=1, sticky=E, pady=10)
+        #style
+        self.email_entry.configure(bd=2, fg="#f00", width= 30)
+        #handle events
+        self.email_entry.bind("<KeyRelease>", self.image_change)
+
+    def image_change(self,event):
+        email= self.email_entry.get()
+        if len(email) == 0:
+            self.image_label.configure(image=self.red_image)
+        else:
+            self.image_label.configure(image=self.green_image)
+
+    def __add_image_label(self):
+        #create
+        self.image_label= Label(self.email_frame)
+        self.image_label.grid(row=2, column=2, padx=10)
+        #style
+        self.image_label.configure(image=self.grey_image, height=20, width=20, )
+        #handle events
+        
+    def __add_type_frame(self):
+        self.type_frame= Frame()
+        self.type_frame.grid(row=3, columnspan=3,sticky=W+E+S+N)
+
+    def __add_type_label(self):
+        self.type_label= Label(self.type_frame)
+        self.type_label.grid(row=3, column=1)
+        self.type_label.configure(text="Type", padx=10, pady=10)
     
-    def add_heading_label(self):
-        self.heading_label=Label(self.outer_frame)
-        self.heading_label.grid(row=0, column=0, columnspan=2)
-        self.heading_label.configure(text="RECEIVE OUR NEWSLETTER", font="Arial 14", pady=10)
-
-    def add_instruction_label(self):
-        self.instruction_label=Label(self.outer_frame)
-        self.instruction_label.grid(row=1, column=0, columnspan=2, sticky=W)
-        self.instruction_label.configure(text="Please enter your email bellow to receive our newsletter.", pady=10, padx=10)
-
-    def add_email_label(self):
-        self.email_label=Label(self.outer_frame)
-        self.email_label.grid(row=2, column=0, sticky=W)
-        self.email_label.configure(text="Email:", padx=15, pady=10)
-
-    def add_email_entry(self):
-        self.email_entry=Entry(self.outer_frame)
-        self.email_entry.grid(row=2, column=0, sticky=E, padx=10)
-        self.email_entry.configure(width=30, fg="#f00")
-
-        self.email_entry.bind("<KeyRelease>", self.image_switch)
-
-    def add_field_image_label(self):
-        self.field_image_label=Label(self.outer_frame)
-        self.field_image_label.grid(row=2, column=1, sticky=W)
-        self.field_image_label.configure(image=self.field_image)
-
-    def add_subscribe_button(self):
-        self.subscribe_button=Button(self.outer_frame)
-        self.subscribe_button.grid(row=3, column=0, columnspan=2, sticky=N+W+S+E)
-        self.subscribe_button.configure(bg="#fee", text="Subscribe")
-
+    def __add_type_optionmenu(self):
+        self.type_optionmenu= OptionMenu(self.type_frame, self.options, "Weekly", "Monthly", "Yearly")
+        self.type_optionmenu.grid(row=3, column=2, padx=10)
+        
+    def __add_subscribe_button(self):
+        #create
+        self.subscribe_button=Button()
+        self.subscribe_button.grid(row=4, columnspan=3, sticky=W+E+S+N)
+        #style
+        self.subscribe_button.configure(text="Subscribe", bg="#fee")
+        #handle events
         self.subscribe_button.bind("<ButtonRelease-1>", self.subscribe_button_clicked)
 
-    def image_switch(self, event):
-        if (len(self.email_entry.get()) > 0):
-            self.field_image_label.configure(image=self.field_image_green)
-
-        else:
-            self.field_image_label.configure(image=self.field_image_red)
-
     def subscribe_button_clicked(self, event):
-        messagebox.showinfo("Newsletter", "Susbcribed!")
+        if len(self.email_entry.get()) >= 1:
+            if (self.options.get() == "Weekly"):
+                messagebox.showinfo("Newsletter", "You have subscribed to the weekly newsletter!")
+            elif(self.options.get() == "Monthly"):
+                messagebox.showinfo("Newletter", "You have subscribed to the monthly newsletter! You will receive this on the first day of each month.")
+            elif(self.options.get() == "Yearly"):
+                messagebox.showinfo("Newletter", "You have subscribed to the yearly newsletter! You will receive this at the start of each year.")
+            else:
+                messagebox.showinfo("Newletter", "Error, please try again.")
+        else:
+            messagebox.showinfo("Newletter", "Please enter your email!")   
+            
+    
 
-
-if __name__ == "__main__":
-    my_gui = TCA()
-    my_gui.mainloop()
-        
+# Create an object of the Gui class when this module is executed
+if (__name__ == "__main__"):
+    gui = Gui()
+    gui.mainloop()
